fcb2b71cd175c980adf62640be64c98211f29afe
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-11-30 18:15:21 -0600

    Refactor order_controller and order_routes

--- controllers/order_controller.py
+++ controllers/order_controller.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify,request
-from controllers.models_folder.models import orders,db
+from flask import Blueprint, jsonify, request
+from controllers.models_folder.models import orders, db
 
-order_controller = Blueprint('order_controller', __name__)
+order_controller = Blueprint("order_controller", __name__)
 
-@order_controller.route('/orders', methods=['GET'])
+
+@order_controller.route("/orders", methods=["GET"])
 def get_all_orders():
     # Obtener todas las órdenes de la base de datos
     all_orders = orders.query.all()
@@ -14,10 +15,29 @@ def get_all_orders():
     # Devolver las órdenes como una respuesta JSON
     return jsonify(dicOrders)
 
-@order_controller.route('/orders', methods=['POST'])
+
+@order_controller.route("/orders", methods=["POST"])
 def create_order():
     data = request.get_json()
     new_order = orders(**data)
     db.session.add(new_order)
     db.session.commit()
-    return jsonify({'message': 'Orden created successfully', 'Order': new_order.to_dict()}), 201
+    return (
+        jsonify(
+            {"message": "Order created successfully", "Order": new_order.to_dict()}
+        ),
+        201,
+    )
+
+@order_controller.route("/orders/<int:order_id>", methods=["DELETE"])
+def delete_order_by_id(order_id):
+    order = orders.query.get(order_id)
+
+    if not order:
+        return jsonify({"message": "Orden no encontrada"}), 404
+    # Si la orden existe, eliminarla y confirmar la transacción
+    db.session.delete(order)
+    db.session.commit()
+
+    # Devolver un mensaje de éxito
+    return jsonify({"message": "Order deleted successfully"}), 200
--- routes/order_routes.py
+++ routes/order_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from controllers.order_controller import get_all_orders,create_order
+from controllers.order_controller import get_all_orders,create_order,delete_order_by_id
 
 order_routes = Blueprint('order_routes', __name__)
 
@@ -10,3 +10,7 @@ def get_orders():
 @order_routes.route('/orders', methods=['POST'])
 def post_order():
     return create_order()
+
+@order_routes.route('/orders/<int:order_id>', methods=['DELETE'])
+def delete_order(order_id):
+    return delete_order_by_id(order_id)
